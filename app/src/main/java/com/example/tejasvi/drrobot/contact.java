bb946e8ac3e14e41399125db25c8b9ef643dafef
pct960 <pct960@gmail.com>
2017-08-27 19:06:23 +0530

    dummy commit

--- app/src/main/java/com/example/tejasvi/drrobot/contact.java
+++ app/src/main/java/com/example/tejasvi/drrobot/contact.java
@@ -6,6 +6,12 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * Created by thbr on 25/8/17.
  */
@@ -16,6 +22,7 @@ public class contact extends Fragment
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         final View v = inflater.inflate(R.layout.layout_contact, container, false);
+
         return v;
     }
 
